272623645c9b0afced384882cd37d70d0e4fe7f1
chct9zw8d2-coder <chct9zw8d2@privaterelay.appleid.com>
2026-03-16 20:39:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-import os
-import requests
 from flask import Flask, request, jsonify
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-API = "https://platform-api.max.ru"
-
-HEADERS = {
-    "Authorization": f"Bearer {BOT_TOKEN}",
-    "Content-Type": "application/json"
-}
-
 app = Flask(__name__)
-
-
-def send_message(chat_id, text):
-
-    payload = {
-        "chat_id": chat_id,
-        "text": text
-    }
-
-    r = requests.post(
-        f"{API}/messages/send",
-        headers=HEADERS,
-        json=payload
-    )
-
-    print("SEND:", r.status_code, r.text)
-
 
 @app.route("/")
 def home():
     return "MAX BOT ONLINE"
-
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -41,20 +12,20 @@ def webhook():
     data = request.json
     print("EVENT:", data)
 
-    message = data.get("message")
+    try:
+        message = data["message"]["text"].lower()
+        chat_id = data["message"]["chat_id"]
 
-    if not message:
-        return jsonify({"ok": True})
+        if message == "привет":
+            return jsonify({
+                "chat_id": chat_id,
+                "text": "привет"
+            })
 
-    text = message.get("text", "").lower()
-    chat_id = message["chat"]["chat_id"]
+    except:
+        pass
 
-    if text == "привет":
-        send_message(chat_id, "привет")
-
-    return jsonify({"ok": True})
-
+    return "ok"
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
